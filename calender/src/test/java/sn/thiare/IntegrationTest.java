c6b649c8696ddec3610450cdfc3f630d340d0007
thiareMohamed <thiaremohamed.mt@gmail.com>
2024-04-03 19:34:01 +0000

    Mise à jour du code

---
+++ calender/src/test/java/sn/thiare/IntegrationTest.java
@@ -0,0 +1,19 @@
+package sn.thiare;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import sn.thiare.config.AsyncSyncConfiguration;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { CalenderApp.class, AsyncSyncConfiguration.class })
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
---
+++ calender/src/test/java/sn/thiare/security/jwt/AuthenticationIntegrationTest.java
@@ -0,0 +1,29 @@
+package sn.thiare.security.jwt;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import sn.thiare.config.SecurityConfiguration;
+import sn.thiare.config.SecurityJwtConfiguration;
+import sn.thiare.config.WebConfigurer;
+import sn.thiare.management.SecurityMetersService;
+import tech.jhipster.config.JHipsterProperties;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(
+    classes = {
+        JHipsterProperties.class,
+        WebConfigurer.class,
+        SecurityConfiguration.class,
+        SecurityJwtConfiguration.class,
+        SecurityMetersService.class,
+        JwtAuthenticationTestUtils.class,
+    }
+)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface AuthenticationIntegrationTest {
+}
